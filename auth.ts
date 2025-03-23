024ec9fdabfe5e4f948e1b2684e9f46d8199bc79
Sam Strike <sam.strike1@gmail.com>
2025-03-23 19:45:48 +0000

    added session strategy and added id to session

--- auth.ts
+++ auth.ts
@@ -5,6 +5,19 @@ import Google from "next-auth/providers/google"
  
 export const { handlers: {GET, POST}, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(db),
+  session: {strategy: "jwt"},
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) { // User is available during sign-in
+        token.id = user.id
+      }
+      return token
+    },
+    session({ session, token }) {
+      session.user.id  = token.id as string
+      return session
+    },
+  },
   providers: [
         Google({
         clientId: process.env.AUTH_GOOGLE_ID,
